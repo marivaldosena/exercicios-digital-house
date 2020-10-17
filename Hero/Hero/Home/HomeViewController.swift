1a52fea6a7a0e8dfac8004d5ffcd2eccb2c622df
Marivaldo Sena <marivaldo.sena@live.com>
2020-10-17 16:51:13 -0300

    Sketch page list layout.

---
+++ Hero/Hero/Home/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hero
+//
+//  Created by Marivaldo Sena on 17/10/20.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
