80e77894888e6db1c9b65eacfff238603b56bb94
Marivaldo Sena <marivaldo.sena@live.com>
2020-10-18 16:13:17 -0300

    Create xib file for Movie Item.

---
+++ Movies/Movies/Home/MovieItemCell/MovieItemTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MovieItemTableViewCell.swift
+//  Movies
+//
+//  Created by Marivaldo Sena on 17/10/20.
+//
+
+import UIKit
+
+class MovieItemTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
