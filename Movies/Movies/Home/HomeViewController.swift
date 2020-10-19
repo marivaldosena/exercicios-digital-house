f0faeac8cb1cb4e3cbdd4c498b529c1e65e6725d
Marivaldo Sena <marivaldo.sena@live.com>
2020-10-18 21:02:27 -0300

    Implement search functionality in Home screen.

--- Movies/Movies/Home/HomeViewController.swift
+++ Movies/Movies/Home/HomeViewController.swift
@@ -13,11 +13,13 @@ class HomeViewController: UIViewController {
     @IBOutlet weak var addMovieBarButtonItem: UIBarButtonItem?
     @IBOutlet weak var movieTableView: UITableView?
     
-    private var movies = [
+    private var allMovies = [
         Movie(title: "Avengers: Endgame", category: "Action", image: "AvengersEndgame", starCount: 8),
         Movie(title: "Avengers: Infinity War", category: "Action", image: "AvengersInfinityWar", starCount: 8),
         Movie(title: "The Avengers", category: "Action", image: "Avengers", starCount: 8)
     ]
+    
+    private var filteredMovies: [Movie] = []
     
     @IBAction func addMovie(_ sender: UIBarButtonItem) {
         self.goToAddMovie()
@@ -30,9 +32,11 @@ class HomeViewController: UIViewController {
         movieTableView?.dataSource = self
         
         movieTableView?.register(UINib(nibName: "MovieItemTableViewCell", bundle: nil), forCellReuseIdentifier: "MovieItemCell")
+        
+        filteredMovies.append(contentsOf: allMovies)
+        
+        movieSearchBar?.delegate = self
     }
-
-
 }
 
 //MARK: - HomeViewController
@@ -53,7 +57,7 @@ extension HomeViewController: UITableViewDelegate {
             return
         }
         
-        viewController.setMovieItem(movies[indexPath.row])
+        viewController.setMovieItem(filteredMovies[indexPath.row])
         navigationController?.pushViewController(viewController, animated: true)
     }
 }
@@ -61,16 +65,46 @@ extension HomeViewController: UITableViewDelegate {
 //MARK: -  HomeViewController: UITableViewDataSource
 extension HomeViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return movies.count
+        return filteredMovies.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "MovieItemCell", for: indexPath) as! MovieItemTableViewCell
         
-        let movie = movies[indexPath.row]
+        let movie = filteredMovies[indexPath.row]
         
         cell.configure(with: movie)
         
         return cell
+    }
+}
+
+extension HomeViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if let term = searchBar.text {
+            self.doSearch(term: term)
+        }
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        guard let term = searchBar.text else {
+            return
+        }
+        
+        self.doSearch(term: term)
+    }
+    
+    private func doSearch(term: String) {
+        filteredMovies = self.getFilteredMovies(term: term)
+        
+        movieTableView?.reloadData()
+    }
+    
+    private func getFilteredMovies(term: String) -> [Movie] {
+        if !term.isEmpty {
+            return allMovies.filter { $0.title.lowercased().contains(term.lowercased()) }
+        }
+        
+        return allMovies
     }
 }
